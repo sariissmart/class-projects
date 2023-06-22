a49458bf2e4e5261a7352dfe6d0f2f05873faf6a
sariissmart <136731351+sariissmart@users.noreply.github.com>
2023-06-22 23:54:08 +0300

    Create lab 8 .py

---
+++ lab 8 .py
@@ -0,0 +1,17 @@
+d = {
+    "name" : "sari",
+    "age" : 11,
+    "city": "riyadh",
+}
+
+print(d['name'])
+print(d['age'])
+print(d['city'])
+d["contrey" ] = "KINGDOM OF SAUDI ARABIA"
+d['age'] = 1990
+
+print(d.keys())
+print(d)
+print(d.values())
+print(d.items())
+
